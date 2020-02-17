6d323bd2a6bbbdd7ffff9ade5e5c0d0b8a26f2f1
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 06:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,28 +2,16 @@ function scuberGreetingForFeet(distance){
   let result 
 
   if (distance <= 400) {
-    result = 'This one is on me!';
+    return 'This one is on me!';
   } else if (distance > 2000 && distance <= 2500) {
-    'I will gladly take your thirty bucks.';
+    return 'I will gladly take your thirty bucks.';
   } else if (distance > 2500) {
-    'No can do.'
+    return 'No can do.'
   }
   return result 
 }
 
- let result
-  if (distance <= 400) {
-    result = "This one is on me!";
-  }
-  else if (distance > 2000 && distance <= 2500) {
-    result = "I will gladly take your 30 bucks."
-  }
-  else if (distance > 2500){
-    result = "No can do."
-  }
-  return result
-}
-
+ 
 function ternaryCheckCity(){
   // Write your code here!
 }
