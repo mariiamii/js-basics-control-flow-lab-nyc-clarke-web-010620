5900abad7b0901fa1b0ddf9658b80bc3f05962cc
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 06:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,20 +18,7 @@ function switchOnCharmFromTip(tip){
       return 'Thank you so much.';
     case 'not as generous':
       return 'Thank you.';
-    case 
+    default: 
       return 'Bye.';
   }
 }
-  
-  describe('switchOnCharmFromTip()', function () {
-    it('should return "Thank you so much." if the tip is generous', function () {
-      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
-    });
-
-    it('should return "Thank you." if the tip is not as generous', function () {
-      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
-    });
-
-    it('should return "Bye." if anything else', function () {
-      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
-    });
