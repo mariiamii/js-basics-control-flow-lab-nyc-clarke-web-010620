7c19bb5f0a0787e6253b74f58c039169aab70370
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 06:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ function scuberGreetingForFeet(distance){
   }
 }
  
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-  
+  city = 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+
+describe('ternaryCheckCity()', function () {
+    it('returns "Ok, sounds good." when the city is NYC', function () {
+      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+    });
+
+    it('should return "No go." if the destination city is not NYC', function () {
+      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+    });
+  });
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
