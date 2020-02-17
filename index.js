d8e57749e169dd7867318fa974ff868b8840c94f
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-17 06:22:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function scuberGreetingForFeet(distance){
-  if (distance <= 400) {
+  if (distance <= 400){
     return 'This one is on me!';
-  } else if (distance > 2000 && distance <= 2500) {
+  } else if (distance > 2000 && distance <= 2500){
     return 'I will gladly take your thirty bucks.';
-  } else if (distance > 2500) {
+  } else if (distance > 2500){
     return 'No can do.'
   }
-  return result 
 }
 
  
